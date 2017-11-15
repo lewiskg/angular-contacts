419a76938c5b4cdfdf1b099399d53764d376f697
lewiskg <kenneth.g.lewis2@gmail.com>
2017-11-15 15:46:37 -0600

    part2 done

--- javascripts/controllers/ViewCtrl.js
+++ javascripts/controllers/ViewCtrl.js
@@ -1,5 +1,6 @@
 'use strict';
 
 app.controller("ViewCtrl", function($scope){
-	$scope.controller = "ViewCtrl";
+	$scope.controller = "Hello, ViewCtrl. Route:/contacts/view";
+	console.log("Hello, NewCtrl. Route:/contacts/view");
 });
