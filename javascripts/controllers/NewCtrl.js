87e31309dc9aed7a0a9121a9404a299ea532e79a
lewiskg <kenneth.g.lewis2@gmail.com>
2017-11-18 06:53:23 -0600

    delete functionality works

--- javascripts/controllers/NewCtrl.js
+++ javascripts/controllers/NewCtrl.js
@@ -4,3 +4,7 @@ app.controller("NewCtrl", function($scope) {
 	$scope.controller = "Hello, NewCtrl. Route:/contacts/new";
 	console.log("Hello, NewCtrl. Route:/contacts/new");
 });
+
+app.controller("checkForm", function($scope) {
+	console.log("in checkForm");
+});
--- javascripts/controllers/ViewCtrl.js
+++ javascripts/controllers/ViewCtrl.js
@@ -1,6 +1,31 @@
 'use strict';
 
-app.controller("ViewCtrl", function($scope){
+app.controller("ViewCtrl", function($rootScope, $scope, ContactService){
 	$scope.controller = "Hello, ViewCtrl. Route:/contacts/view";
 	console.log("Hello, NewCtrl. Route:/contacts/view");
+
+	$scope.contacts = [];
+
+	const getMyContacts = () => {	
+		ContactService.getContacts($rootScope.uid).then((results) => {
+			$scope.contacts = results;
+		}).catch((err) => {
+			console.log("error in getMyContacts", err);
+		});
+	};
+
+	getMyContacts();
+
+	$scope.deleteMyContact = (contactId) => {
+		ContactService.deleteContact(contactId).then((results) => {
+			console.log("results", results);
+			getMyContacts();
+		}).catch((err) => {
+			console.log("error in deleteMyContacts", err);
+		});
+	};
+
+
+
+
 });
